0a557b5307d3caa2fb6b7eab0dc515e498e9aadb
webbywolf <info.webbywolf@gmail.com>
2020-12-24 12:00:22 +0530

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div>
+        <p>Made with ❤️️ by Karan Rajput...</p>
+        <p>
+          ...Powered by{" "}
+          <a target="_blank" href="https://www.pixabay.com">
+            Pixabay Api
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
